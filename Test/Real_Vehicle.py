ac0fd45866705d84204e8435edff4f1e9391b265
ayushsingh11 <30299945+ayushsingh11@users.noreply.github.com>
2020-04-26 00:23:53 +0530

    Add files via upload

---
+++ Test/Real_Vehicle.py
@@ -0,0 +1,59 @@
+__author__ = 'Ayush Singh'
+
+import serial
+import pygame
+from pygame.locals import *
+
+
+class RCTest(object):
+
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_mode((250, 250))
+        #self.ser = serial.Serial("/dev/cu.HC05_SLAVE-DevB", 115200, timeout=1)    # mac_bluetooth
+        self.ser = serial.Serial("/dev/cu.usbmodem14201", 115200, timeout=1)    # mac
+        # self.ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)           # linux
+        self.send_inst = True
+        self.steer()
+
+    def steer(self):
+
+        while self.send_inst:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    key_input = pygame.key.get_pressed()
+
+                    
+
+                    # simple orders
+                    if key_input[pygame.K_UP]:
+                        print("Forward")
+                        self.ser.write(chr(1).encode())
+
+                    elif key_input[pygame.K_DOWN]:
+                        print("Reverse")
+                        self.ser.write(chr(2).encode())
+
+                    elif key_input[pygame.K_RIGHT]:
+                        print("Right")
+                        self.ser.write(chr(3).encode())
+
+                    elif key_input[pygame.K_LEFT]:
+                        print("Left")
+                        self.ser.write(chr(4).encode())
+
+                    # exit
+                    elif key_input[pygame.K_x] or key_input[pygame.K_q]:
+                        print("Exit")
+                        self.send_inst = False
+                        self.ser.write(chr(0).encode())
+                        self.ser.close()
+                        break
+
+                elif event.type == pygame.KEYUP:
+                    self.ser.write(chr(0).encode())
+
+
+if __name__ == '__main__':
+    RCTest()
+
